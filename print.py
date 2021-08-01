04cbe48b24d88b0159d8fde365a657c4e12e5f70
yumin97 <fodleldjs@naver.com>
2021-08-01 19:04:56 +0900

    print

---
+++ print.py
@@ -0,0 +1,18 @@
+def solution(priorities, location):
+    answer = 0
+    
+    array1 = [c for c in range(len(priorities))]
+    array2 = priorities.copy()
+    
+    i = 0
+    while True:
+        if array2[i] < max(array2[i+1:]):
+            array1.append(array1.pop(i))
+            array2.append(array2.pop(i))
+        else:
+            i += 1
+            
+        if array2 == sorted(array2, reverse=True):
+            break
+            
+    return array1.index(location) + 1
